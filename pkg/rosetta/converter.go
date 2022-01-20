098f96830e87e9410ad1e0ca2fc8cdccea27a8e4
ross-weir <29697678+ross-weir@users.noreply.github.com>
2022-01-20 21:57:23 +1100

    add rosetta package with ergo converter

---
+++ pkg/rosetta/converter.go
@@ -0,0 +1,224 @@
+package rosetta
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/coinbase/rosetta-sdk-go/types"
+	"github.com/ross-weir/rosetta-ergo/pkg/ergo"
+	ergotype "github.com/ross-weir/rosetta-ergo/pkg/ergo/types"
+)
+
+type Converter struct {
+	e          *ergo.Client
+	inputCoins map[string]*types.AccountCoin
+}
+
+func (c *Converter) BlockToRosettaBlock(
+	ctx context.Context,
+	block *ergotype.FullBlock,
+) (*types.Block, error) {
+	b, err := c.HeaderToRosettaBlock(ctx, block.Header)
+	if err != nil {
+		return nil, err
+	}
+
+	ergoTxs := *block.BlockTransactions.Transactions
+	parsedTxs := make([]*types.Transaction, len(ergoTxs))
+
+	for idx, ergoTx := range ergoTxs {
+		parsedTx, err := c.TxToRosettaTx(ctx, &ergoTx)
+		if err != nil {
+			return nil, fmt.Errorf(
+				"%w: error parsing transaction operations, txId: %s",
+				err,
+				*ergoTx.ID,
+			)
+		}
+
+		parsedTxs[idx] = parsedTx
+
+		// store outputs for this tx in `c.inputCoins`
+		// incase they're used in later txs
+		c.storeTxCoins(parsedTx)
+	}
+
+	return b, nil
+}
+
+func (c *Converter) HeaderToRosettaBlock(
+	ctx context.Context,
+	bh *ergotype.BlockHeader,
+) (*types.Block, error) {
+	blockIndex := int64(bh.Height)
+	previousBlockIndex := blockIndex - 1
+	previousBlockHash := bh.ParentID
+
+	// the genesis blocks parent is itself according to the rosetta spec
+	if blockIndex == genesisBlockIndex {
+		previousBlockIndex = genesisBlockIndex
+		previousBlockHash = bh.ID
+	}
+
+	return &types.Block{
+		BlockIdentifier: &types.BlockIdentifier{
+			Hash:  bh.ID,
+			Index: blockIndex,
+		},
+		ParentBlockIdentifier: &types.BlockIdentifier{
+			Hash:  previousBlockHash,
+			Index: previousBlockIndex,
+		},
+		Timestamp: bh.Timestamp,
+	}, nil
+}
+
+func (c *Converter) TxToRosettaTx(
+	ctx context.Context,
+	ergoTx *ergotype.ErgoTransaction,
+) (*types.Transaction, error) {
+	txOps := []*types.Operation{}
+	inputs := *ergoTx.Inputs
+
+	for inputIdx, input := range inputs {
+		acctCoin, ok := c.inputCoins[input.BoxID]
+		if !ok {
+			return nil, fmt.Errorf("unable to find inputt %s, for tx: %s", input.BoxID, *ergoTx.ID)
+		}
+
+		txOp, err := c.InputToRosettaOp(
+			&inputs[inputIdx],
+			int64(len(txOps)),
+			int64(inputIdx),
+			acctCoin,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("%w: error parsing tx input, boxId: %s", err, input.BoxID)
+		}
+
+		txOps = append(txOps, txOp)
+	}
+
+	outputs := *ergoTx.Outputs
+	for outputIdx, output := range outputs {
+		txOp, err := c.OutputToRosettaOp(ctx, &outputs[outputIdx], int64(len(txOps)))
+		if err != nil {
+			return nil, fmt.Errorf("%w: error parsing tx output, boxId: %s", err, *output.BoxID)
+		}
+
+		txOps = append(txOps, txOp)
+	}
+
+	// TODO: metadata?
+	return &types.Transaction{
+		TransactionIdentifier: &types.TransactionIdentifier{
+			Hash: *ergoTx.ID,
+		},
+		Operations: txOps,
+	}, nil
+}
+
+func (c *Converter) InputToRosettaOp(
+	i *ergotype.ErgoTransactionInput,
+	operationIndex int64,
+	inputIndex int64,
+	inputCoin *types.AccountCoin,
+) (*types.Operation, error) {
+	negatedVal, err := types.NegateValue(inputCoin.Coin.Amount.Value)
+	if err != nil {
+		return nil, fmt.Errorf("%w: unable to negate previous output", err)
+	}
+
+	return &types.Operation{
+		OperationIdentifier: &types.OperationIdentifier{
+			Index:        operationIndex,
+			NetworkIndex: &inputIndex,
+		},
+		Type:    InputOpType,
+		Status:  types.String(SuccessStatus),
+		Account: inputCoin.Account,
+		Amount: &types.Amount{
+			Value:    negatedVal,
+			Currency: Currency,
+		},
+		CoinChange: &types.CoinChange{
+			CoinIdentifier: &types.CoinIdentifier{
+				Identifier: i.BoxID,
+			},
+			CoinAction: types.CoinSpent,
+		},
+		// metadata
+	}, nil
+}
+
+func (c *Converter) OutputToRosettaOp(
+	ctx context.Context,
+	o *ergotype.ErgoTransactionOutput,
+	operationIndex int64,
+) (*types.Operation, error) {
+	coinChange := &types.CoinChange{
+		CoinIdentifier: &types.CoinIdentifier{
+			Identifier: *o.BoxID,
+		},
+		CoinAction: types.CoinCreated,
+	}
+
+	addr, err := c.e.TreeToAddress(ctx, o.ErgoTree)
+	if err != nil {
+		return nil, fmt.Errorf("%w: failed to get address from ergo tree", err)
+	}
+
+	account := &types.AccountIdentifier{Address: *addr}
+	intBase := 10
+
+	return &types.Operation{
+		OperationIdentifier: &types.OperationIdentifier{
+			Index:        operationIndex,
+			NetworkIndex: types.Int64(int64(*o.Index)),
+		},
+		Type:    OutputOpType,
+		Status:  types.String(SuccessStatus),
+		Account: account,
+		Amount: &types.Amount{
+			Value:    strconv.FormatInt(o.Value, intBase),
+			Currency: Currency,
+		},
+		CoinChange: coinChange,
+		// Metadata
+	}, nil
+}
+
+func (c *Converter) PeerToRosettaPeer(p *ergotype.Peer) (*types.Peer, error) {
+	metadata, err := types.MarshalMap(p)
+	if err != nil {
+		return nil, fmt.Errorf("%w: unable to marshal peer metadata", err)
+	}
+
+	return &types.Peer{
+		PeerID:   p.Address,
+		Metadata: metadata,
+	}, nil
+}
+
+// Preserve input coins used in transactions so they can
+// be used for later transactions in the block if required
+func (c *Converter) storeTxCoins(tx *types.Transaction) {
+	for _, op := range tx.Operations {
+		if op.CoinChange == nil {
+			continue
+		}
+
+		if op.CoinChange.CoinAction != types.CoinCreated {
+			continue
+		}
+
+		c.inputCoins[op.CoinChange.CoinIdentifier.Identifier] = &types.AccountCoin{
+			Coin: &types.Coin{
+				CoinIdentifier: op.CoinChange.CoinIdentifier,
+				Amount:         op.Amount,
+			},
+			Account: op.Account,
+		}
+	}
+}
---
+++ pkg/rosetta/types.go
@@ -0,0 +1,62 @@
+package rosetta
+
+import "github.com/coinbase/rosetta-sdk-go/types"
+
+const (
+	genesisBlockIndex = 1
+
+	// The rosetta blockchain is Ergo.
+	Blockchain string = "Ergo"
+
+	// The identifier for the mainnet network identifier in rosetta
+	MainnetNetwork string = "mainnet"
+
+	// The identifier for the testnet network identifier in rosetta
+	TestnetNetwork string = "testnet"
+
+	ErgDecimals = 9
+
+	// The amount of nano ergs = 1 erg
+	NanoErgInErg = 1000000000
+
+	// Input op type represents a UTXO input in a transaction
+	InputOpType = "INPUT"
+
+	// Output op type represents a UTXO output in a transaction
+	OutputOpType = "OUTPUT"
+)
+
+var (
+	MainnetGenesisBlockIdentifier = &types.BlockIdentifier{
+		Index: 1,
+		Hash:  "b0244dfc267baca974a4caee06120321562784303a8a688976ae56170e4d175b",
+	}
+
+	TestnetGenesisBlockIdentifier = &types.BlockIdentifier{
+		Index: 1,
+		Hash:  "78bc468ff36d5d6fa9707d382fe5b8947d6dd0ed2bc5c44707426ee12557c7fb",
+	}
+
+	Currency = &types.Currency{
+		Symbol:   "ERG",
+		Decimals: ErgDecimals,
+	}
+
+	OperationTypes = []string{
+		InputOpType,
+		OutputOpType,
+	}
+
+	// SuccessStatus is the status of all
+	// Ergo operations because anything
+	// on-chain is considered successful.
+	SuccessStatus = "SUCCESS"
+
+	// OperationStatuses are all supported operation.Status.
+	OperationStatuses = []*types.OperationStatus{
+		{
+			Status:     SuccessStatus,
+			Successful: true,
+		},
+	}
+)
